590e245a8a1d460bd3f4b575bd75745748d48863
Gidion Khaemba <gidionkhaemba@gmail.com>
2024-09-03 00:17:56 +0300

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+
+# Create your views here.
+
+def login_view(request):
+    if request.method=="POST":        
+        login_form=AuthenticationForm(request=request, data=request.POST)
+        if login_form.is_valid():
+            username=login_form.cleaned_data.get('username')
+            password=login_form.cleaned_data.get('password')
+            user=authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+                pass
+            else:
+                pass
+    elif request.method=='GET':
+        login_form=AuthenticationForm()       
+    context= {"login_form":login_form}
+    return render(request,'views/login.html', context)
