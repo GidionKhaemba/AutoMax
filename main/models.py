03609d907699b1896a14bc475a1c861ad61ca486
Gidion Khaemba <gidionkhaemba@gmail.com>
2024-09-08 02:06:48 +0300

    made few changes

--- main/models.py
+++ main/models.py
@@ -21,3 +21,5 @@ class Listing(models.Model):
     location=models.OneToOneField(Location, on_delete=models.SET_NULL, null=True )
     image=models.ImageField(upload_to=user_listing_path)
     
+    def __str__(self):
+        return f'{self.seller.user.username}\'s Listing - {self.model}'
