bc45d0341be65288f26ccdb551ce3e6067b43a30
Gidion Khaemba <gidionkhaemba@gmail.com>
2024-09-08 20:51:59 +0300

    created listings cards

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Listing
 
 @login_required
 def home_view(request):
-    return render(request, 'views/home.html')
+    listings=Listing.objects.all()
+    context={"listings":listings}
+    return render(request, 'views/home.html', context)
 
 
 def main_view(request):
